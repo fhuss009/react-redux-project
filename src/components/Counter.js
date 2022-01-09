10d5911faf39a372f740d393033d2e3e4c66d019
fhuss009 <fhuss009@fiu.edu>
2022-01-08 22:33:02 -0600

    Adding increasing, and decreasing counter functionality

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,27 @@
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREASE_COUNTER" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DECREASE_COUNTER" });
+  };
   const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
